06ea5e62e9b53c7ddc589b19908bb26d0cd18222
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-27 17:16:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ // get the element where the time is to be displayed
+	let timerDisplay=document.getElementById("timer");
+
+//set the current date and time 
+let currentDate=new Date();
+
+//update the time in every second 
+setInterval(
+	function(){
+		//get the current date and time 
+		currentDate=new Date();
+
+		//update the timer Display 
+		timerDisplay.innerHTML=currentDate.tolocaleString();
+		
+	},1000
+);
